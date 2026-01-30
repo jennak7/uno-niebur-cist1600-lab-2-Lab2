e93ef52e20aa7d60bf3cdb75fd1eb991e0338ad2
jennak7 <jennakramer727@gmail.com>
2026-01-29 22:38:54 -0600

    Completed Lab 2

--- FutureTime.py
+++ FutureTime.py
@@ -1,7 +1,7 @@
 #FutureTime.py
-#Name:
-#Date:
-#Assignment:
+#Name: Jenna Kramer
+#Date: 01/28/26
+#Assignment: Lab 2
 
 # datetime will allow us to access the system date and time.
 import datetime
@@ -9,20 +9,36 @@ import datetime
 def main():
   #getting current time from system, storing to variable
   now = datetime.datetime.now()
-  currentHour = now.hour
+  currentHour = now.hour % 24
   currentMinute = now.minute
 
-  print (currentHour, currentMinute) #this is just for checking, we should delete it later
+  #print (currentHour, currentMinute) #this is just for checking, we should delete it later
 
   #TODO:
   #Ask user for hours
+  moreHours = int(input("How many hours in the future would you like me to calculate?"))
+
+
   #Ask user for minutes
+  moreMinutes = int(input("How many minutes in the future would you like me to calculate?")) 
+ 
 
   #Calculate the time after the user-supplied time has passed.
+  futureMinutes = (currentMinute + moreMinutes) % 60
+  extraHour = (currentMinute + moreMinutes) // 60
+  futureHour = (currentHour + moreHours + extraHour) % 24
 
   #Do not use any if statements in calculating the time.
-
   #Output the future time in standard format "HH:MM"
+  formattedFutureHour = str(futureHour) 
+  if futureHour < 10: 
+    formattedFutureHour = "0" + str(futureHour)
+  formattedFutureMinutes = str(futureMinutes)
+  if futureMinutes < 10: 
+    formattedFutureMinutes = "0" + str(futureMinutes)
+  
+
+  print("The future time is", formattedFutureHour, ":", formattedFutureMinutes)
 
 
 if __name__ == '__main__':
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,7 +1,7 @@
 #Magic8Ball.py
-#Name:
-#Date:
-#Assignment:
+#Name: Jenna Kramer
+#Date: 01/28/26
+#Assignment: Lab 2
 
 #We will need random for this program, import to use this package.
 import random
@@ -10,9 +10,17 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
-
+  input("Ask me a yes or no question.")
+  answers = ("Yes", "No", "Perhaps", "Not yet", "In the near future", "Never", "No one knows", "Patience, little one", 
+             "Ask again later", "You must donate to my gofundme for clarity", "Reflect upon yourself", "The future is murky", 
+             "Your question needs refining", "Most definitely", "Yikes", "Proceed carefully", "You will see in due time", 
+             "To the highest degree", "Absolutely not", "What a silly question") 
+  
   #Answer question randomly with one of the options from your earlier list.
-
-
+  r = random.random() * len(answers)
+  r = int(r) #cut off any decimal values
+  # print(r)
+  response = answers[r]
+  print(response)
 if __name__ == '__main__':
   main()
